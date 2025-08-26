3189a0fc977b425a5b12f6f99544720ab564a8ef
Erin Telfer <eringtelfer@gmail.com>
2025-08-25 19:11:42 -0500

    site displays cities only once and event listener lists all walkers servicing said city

--- scripts/CityList.js
+++ scripts/CityList.js
@@ -1,17 +1,30 @@
-import { getWalkers } from "./database.js"
+import { getWalkers, getCities } from "./database.js"
 
 const walkers = getWalkers()
+const cities = getCities()
 
 document.addEventListener(
     "click",
     (clickEvent) => {
         const cityTarget = clickEvent.target
 
-        if (cityTarget.dataset.type === "city")
+        if (cityTarget.dataset.type === "city"){
 
-        window.alert(`${cityTarget.dataset.walkername} is servicing this city. `)
+        const idOfCity = parseInt(cityTarget.dataset.id)
+
+        const servicingWalkers = []
+
+        for (const walker of walkers) {
+            
+        if (walker.cityId === idOfCity) {
+            servicingWalkers.push(walker.name)
+        }
 
     }
+        window.alert(`This city is serviced by ${servicingWalkers} `)
+
+    }
+}
 )
 
 export const CityList = () => {
@@ -19,9 +32,9 @@ export const CityList = () => {
 
     citiesHTML += "<ol>"
 
-    for (const walker of walkers) {
-        citiesHTML += `<li data-type="city" data-walkername="${walker.name}"
-                        >${walker.city}</li>`
+    for (const city of cities) {
+        citiesHTML += `<li data-type="city" data-id="${city.id}"
+                        >${city.name}</li>`
     }
 
     citiesHTML += "</ol>"
--- scripts/Walkers.js
+++ scripts/Walkers.js
@@ -1,16 +1,28 @@
-import { getWalkers } from "./database.js"
+import { getWalkers, getCities } from "./database.js"
 
 const walkers = getWalkers()
+const cities = getCities()
 
 document.addEventListener(
     "click",
     (eventAttachedToClick) => {
         const thingClickedOn = eventAttachedToClick.target
+
         if (thingClickedOn.dataset.type === "walker") {
-            window.alert(`This walker works in ${thingClickedOn.dataset.city}`)
+
+        const cityId = thingClickedOn.dataset.cityforeignkey
+
+        const allCities = getCities()
+
+        for (const city of allCities) {
+            
+        if (city.id === parseInt(cityId)) {
+
+            window.alert(`This walker works in ${city.name}`)
         }
-    
+        }
     }
+}
 )
 
 
@@ -21,7 +33,7 @@ export const Walkers = () => {
 
     for (const walker of walkers) {
         walkerHTML += `<li data-id="${walker.id}"
-                            data-city="${walker.city}"
+                            data-cityforeignkey="${walker.cityId}"
                             data-type="walker"
                             data-email="${walker.email}"
                             >${walker.name}</li>`
